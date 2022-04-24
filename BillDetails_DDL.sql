6f35d4e1ee6c4cd9d491691913114962887bcd6d
Rakhul Kumar <Rakhul.Kumar007@gmail.com>
2022-04-24 17:01:01 +0530

    Created Doctor,Room and Bill Tables
    DDL Commands only. More values need to be inserted. Referential Key constraints pending

---
+++ BillDetails_DDL.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `HospitalManagement`.`BILL_DETAILS` (
+  `Test_id` INT NOT NULL,
+  `Bill_no` INT NOT NULL,
+  `Doc_name` VARCHAR(45) NOT NULL,
+  `Doc_id` VARCHAR(45) NULL,
+  `Patient_name` VARCHAR(45) NOT NULL,
+  `Patient_id` INT NOT NULL,
+  `Consultant_fee` INT NULL,
+  `Date` DATETIME NOT NULL,
+  `Test_fee` INT NULL,
+  UNIQUE INDEX `Labtest_id_UNIQUE` (`Test_id` ASC) VISIBLE,
+  PRIMARY KEY (`Bill_no`),
+  UNIQUE INDEX `Bill_no_UNIQUE` (`Bill_no` ASC) VISIBLE);
+  
+  
---
+++ Room_DDL.sql
@@ -0,0 +1,29 @@
+/*  
+CREATE TABLE `HospitalManagement`.`ROOM` (
+  `Room_no` INT NOT NULL,
+  `Ward` VARCHAR(45) NOT NULL,
+  `Type` VARCHAR(45) NOT NULL,
+  `Patient_id` VARCHAR(45) NULL,
+  `Doc_id` int NULL,
+  PRIMARY KEY (`Room_no`),
+  FOREIGN KEY (Doc_id) REFERENCES DOCTOR(Doc_id),
+  UNIQUE INDEX `Room_no_UNIQUE` (`Room_no` ASC) VISIBLE);
+  
+
+ 
+
+INSERT INTO ROOM(Room_no,Ward,Type,Patient_id,Doc_id)
+VALUES
+('1001','G-Ward','NonAC-Double-Sharing','2001','1004'),
+('7001','E-Ward','Executive','2004','1002'),
+('2011','J-Ward','Private','2003','1003'),
+('2001','J-Ward','Semi-Private','2002','1005'),
+('1011','G-Ward','AC-Double-Sharing','2005','1001')
+;
+
+*/ 
+
+SELECT * FROM ROOM;
+
+
+
