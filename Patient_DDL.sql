acfeb1742c6dfb105e0ddf3c0d7974623ae8fb07
Rakhul Kumar <Rakhul.Kumar007@gmail.com>
2022-04-28 22:29:06 +0530

    Reboot Pharmacy Table

--- Patient_DDL.sql
+++ Patient_DDL.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `PATIENT` (
   `Patient_id` INT NOT NULL,
+  `Patient_name` VARCHAR(50) NOT NULL,
   `DOB` DATE NOT NULL,
   `Gender` VARCHAR(45) NOT NULL,
   `Address` VARCHAR(45) NOT NULL,
   `Diagnosis` VARCHAR(45) NOT NULL,
   `PhNumber` VARCHAR(10) NOT NULL,
-  `Patient_name` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`Patient_id`),
   UNIQUE INDEX `Patient_id_UNIQUE` (`Patient_id` ASC) VISIBLE);
    
--- Pharmacy_DDL.sql
+++ Pharmacy_DDL.sql
@@ -1,12 +1,8 @@
-CREATE TABLE IF NOT EXISTS `PHARMACY` (
-  `Bill_number` INT NOT NULL,
-  `Medicine` VARCHAR(45) NOT NULL,
-  `Stock` INT NOT NULL,
-  `Price` INT NOT NULL,
-  `Patient_id` INT NOT NULL,
-  `Doc_id` INT NOT NULL,
-  `Patient_name` VARCHAR(45) NOT NULL,
-  `TotalAmount` INT NOT NULL,
-  PRIMARY KEY (`Bill_number`),
-  UNIQUE INDEX `Bill_number_UNIQUE` (`Bill_number` ASC) VISIBLE,
-  UNIQUE INDEX `Medicine_UNIQUE` (`Medicine` ASC) VISIBLE);
+CREATE TABLE `HospitalManagement`.`PHARMACY` (
+  `Medicine_id` INT NOT NULL,
+  `Medicine_name` VARCHAR(45) NOT NULL,
+  `Quantity` VARCHAR(45) NOT NULL,
+  `Price` VARCHAR(45) NOT NULL,
+  `Expiry_date` VARCHAR(45) NOT NULL,
+  `Storage_temp` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Medicine_id`));
