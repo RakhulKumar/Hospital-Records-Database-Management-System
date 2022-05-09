387d0108f5e556f73b7a90339dc2382e65c822fd
Rakhul Kumar <Rakhul.Kumar007@gmail.com>
2022-05-09 20:42:23 +0530

    Minor changes

--- Patient_DDL.sql
+++ Patient_DDL.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `PATIENT` (
   PRIMARY KEY (`Patient_id`),
   UNIQUE INDEX `Patient_id_UNIQUE` (`Patient_id` ASC) VISIBLE);
    
-
+/*
   insert into PATIENT (Patient_id, DOB, Gender, Address, Diagnosis, PhNumber, Patient_name) values
   (2001, '2001-10-21', 'Male', 'B3-11D Olympia Grande', 'Asthma', '9182736451','Pramodh'),
   (2002, '2001-09-19', 'Male', 'B3-11C Olympia Grande', 'Sore Throat', '927736451', 'Amudhapriyan'),
@@ -20,6 +20,6 @@ CREATE TABLE IF NOT EXISTS `PATIENT` (
   (2007, '2002-01-05', 'Male', '1, Sea Breeze Avenue', 'Kidney Failure', '912896451', 'Premchandran'),  
   (2008, '1994-05-23', 'Female', '24 Highrise Block', 'Borderline Personality Disorder', '918235451', 'Susan')
   ;
-  
+  */
   
   select * from patient
